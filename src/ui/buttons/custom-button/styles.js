188eae3ffa04e768c215dc3ed74098f8c97bc6de
unknown <edlayweb@gmail.com>
2021-04-07 03:17:52 +0300

    refactoring

---
+++ src/ui/buttons/custom-button/styles.js
@@ -0,0 +1,10 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+    }
+}));
+
+export default useStyles;
